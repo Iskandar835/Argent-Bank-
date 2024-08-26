fef49a284b776c75ec9373e960b2eb138eb8981d
Sacha Vandermoeten <sachadu835@gmail.com>
2024-08-26 15:57:15 +0200

    Ajout du Footer et fini

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,16 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home.jsx'
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-    <Header />
+     <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route />
         <Route />
       </Routes>
+     <Footer />
     </Router>
   </React.StrictMode>,
 )
