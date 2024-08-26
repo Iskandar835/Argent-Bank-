6f0aa62755820e1c38514aacbbb9d25ab591dc9c
Sacha Vandermoeten <sachadu835@gmail.com>
2024-08-26 14:54:10 +0200

    Installation de props-types, react-router-dom/ Header en cours/ Supression des fichiers inutiles/ Ancien html gardé pour exemple

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+function Header () {
+    return (
+        <nav className="main-nav">
+            <a className="main-nav-logo" href="">
+                <img
+                    className="main-nav-logo-image" 
+                    src="../../designs/img/argentBankLogo.png" 
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="dr-only">Argent Bank</h1>
+            </a>
+            <div>
+                <a className="main-nav-item" href="">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
+import Home from './pages/Home.jsx'
+import Header from './components/Header.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route />
+        <Route />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
 )
