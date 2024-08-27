6dbf6c5c5bf226fe5e31bc1c6aa8dde05b0fb82a
Sacha Vandermoeten <sachadu835@gmail.com>
2024-08-27 14:46:26 +0200

    Installation et configuration de Redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,29 @@
+// REACT 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// REDUX 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from "./reducers";
 import './assets/index.css';
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
+
+// FILES 
 import Home from './pages/Home.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import SignIn from './pages/Signin.jsx';
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <Router>
      <Header />
@@ -18,5 +34,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Routes>
      <Footer />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
 );
